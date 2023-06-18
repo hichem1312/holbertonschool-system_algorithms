edc07d72bc42c439eb4f613e536e629ae5ce478d
hichem <hichem@hichem.com>
2023-06-18 15:32:58 +0100

    a function that creates a Red-Black Tree node

---
+++ red_black_tree/0-rb_tree_node.c
@@ -0,0 +1,24 @@
+#include "rb_trees.h"
+
+/**
+ * rb_tree_node - a function that creates a Red-Black Tree node
+ * @parent: pointer to the parent node
+ * @value: the value to put in the new node
+ * @color: the color of the node
+ * Return: a pointer to the new node, or NULL on failure
+ */
+
+ rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color)
+{
+    rb_tree_t *node = malloc(sizeof(*node));
+
+	if (node)
+	{
+		node->n = value;
+		node->color = color;
+		node->parent = parent;
+		node->left = NULL;
+		node->right = NULL;
+	}
+    return (node);
+}
---
+++ red_black_tree/rb_trees.h
@@ -0,0 +1,41 @@
+#ifndef _RB_TREES_H_
+#define _RB_TREES_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * enum rb_color_e - possible red-black tree colors
+ *
+ * @RED: 0 -> red node
+ * @BLACK: 1 -> black node
+ * @DOUBLE_BLACK: 2 -> double-black node (used for deletion)
+ */
+typedef enum rb_color_e
+{
+	RED = 0,
+	BLACK,
+	DOUBLE_BLACK
+} rb_color_t;
+
+
+/**
+ * struct rb_tree_s - red-black tree node structure
+ *
+ * @n: Integer stored in the node
+ * @parent: pointer to the parent node
+ * @left: pointer to the left child node
+ * @right: pointer to the right child node
+ * @color: color of the node (RED or BLACK)
+ */
+typedef struct rb_tree_s
+{
+	int n;
+	rb_color_t color;
+	struct rb_tree_s *parent;
+	struct rb_tree_s *left;
+	struct rb_tree_s *right;
+} rb_tree_t;
+rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color);
+#endif /* _RB_TREES_H_ */
